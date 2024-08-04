e0f4f783136768efded4c11ceeacc3eaff808cc9
Mohamed Ashraf <botoashraf123@gmail.com>
2024-08-05 02:11:41 +0300

    chore: add shadui textarea component

---
+++ src/components/ui/textarea.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import * as React from "react";
+
+import { cn } from "@/utils/shadUtils";
+
+const Textarea = React.forwardRef(({ className, ...props }, ref) => {
+  return (
+    <textarea
+      rows={1}
+      className={cn(
+        "flex w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 resize-none overflow-hidden",
+        className
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Textarea.displayName = "Textarea";
+
+export { Textarea };
