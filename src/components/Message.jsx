523bba5c2c7299ff2c1986aed99a9966c32270c1
Mohamed Ashraf <botoashraf123@gmail.com>
2024-08-05 05:09:31 +0300

    feta: add style to messages and wrap it in Markdown component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,13 +1,30 @@
 /* eslint-disable react/prop-types */
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
 export const Message = ({ content, role }) => {
   return (
     <div className={`flex ${role === "user" ? "justify-end" : ""}`}>
+      {/* bot avatar */}
+
+      {role === "bot" && (
+        <div className="flex items-center justify-center w-6 h-6 p-5 bg-muted rounded-full mt-2">
+          GB
+        </div>
+      )}
       <div
-        className={` ${
-          role === "user" ? "bg-primary text-primary-foreground" : "bg-muted"
-        }  py-2 px-3 rounded-lg inline-block text-lg`}
+        className={` ${role === "user" ? "bg-muted text-primary" : ""} ${
+          role === "bot" ? "flex flex-col gap-4" : ""
+        } py-2 px-3 rounded-xl inline-block text-base`}
       >
-        {content}
+        {
+          <Markdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeHighlight]}
+          >
+            {content}
+          </Markdown>
+        }
       </div>
     </div>
   );
