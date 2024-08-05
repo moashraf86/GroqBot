1cb52abc3bec9881a0e25b312005b54df444d738
Mohamed Ashraf <botoashraf123@gmail.com>
2024-08-05 18:49:05 +0300

    refactor: Add code block copy functionality to Message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,25 +2,66 @@
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
+import { useRef } from "react";
+import { CheckIcon, CopyIcon } from "@radix-ui/react-icons";
 export const Message = ({ content, role }) => {
+  const buttonRef = useRef(null);
+
+  /**
+   * Copy code block to clipboard
+   */
+  const handleCopy = () => {
+    navigator.clipboard.writeText(buttonRef.current.nextSibling.innerText);
+    // change the button text for 2s only
+    buttonRef.current.querySelector(".copy").classList.add("hidden");
+    buttonRef.current.querySelector(".copied").classList.remove("hidden");
+    buttonRef.current.querySelector(".copied").classList.add("flex");
+
+    setTimeout(() => {
+      buttonRef.current.querySelector(".copy").classList.remove("hidden");
+      buttonRef.current.querySelector(".copied").classList.add("hidden");
+      buttonRef.current.querySelector(".copied").classList.remove("flex");
+    }, 2000);
+  };
   return (
     <div className={`flex ${role === "user" ? "justify-end" : ""}`}>
       {/* bot avatar */}
-
-      {role === "bot" && (
-        <div className="flex items-center justify-center w-6 h-6 p-5 bg-muted rounded-full mt-2">
-          GB
-        </div>
-      )}
       <div
-        className={` ${role === "user" ? "bg-muted text-primary" : ""} ${
-          role === "bot" ? "flex flex-col gap-4" : ""
-        } py-2 px-3 rounded-xl inline-block text-base`}
+        className={` ${
+          role === "user" ? "bg-muted text-primary rounded-xl" : ""
+        } ${
+          role === "bot" ? "flex flex-col gap-4 border-l border-primary/40" : ""
+        } py-2 px-3 inline-block text-base`}
       >
         {
           <Markdown
             remarkPlugins={[remarkGfm]}
             rehypePlugins={[rehypeHighlight]}
+            components={{
+              pre: ({ node, ...props }) => {
+                return (
+                  <pre {...props}>
+                    <button
+                      title="Copy code"
+                      ref={buttonRef}
+                      onClick={handleCopy}
+                      className="absolute top-4 right-4 px-3 py-1.5 md:bg-background rounded-full text-sm text-primary cursor-pointer"
+                    >
+                      <span className="copy flex gap-2 items-center justify-center">
+                        <CopyIcon className="w-4 h-4" />
+                        <span className="hidden md:inline">copy code</span>
+                      </span>
+
+                      <span className="copied hidden gap-2 items-center justify-center">
+                        <CheckIcon className="w-4 h-4" />
+                        <span className="hidden md:inline">copied</span>
+                      </span>
+                    </button>
+                    {props.children}
+                  </pre>
+                );
+              },
+            }}
           >
             {content}
           </Markdown>
