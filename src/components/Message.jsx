85f4822fb7a85d0619ac7e6270bcf76f74f26442
moashraf86 <botoashraf123@gmail.com>
2024-08-30 16:22:58 +0300

    refactor: Update Message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,18 +1,19 @@
 import MDEditor from "@uiw/react-md-editor";
 export const Message = ({ content, role }) => {
+  console.log(content, role);
+
   return (
     <div className={`flex ${role === "user" ? "justify-end" : ""}`}>
       <div
         className={` ${
           role === "user" ? "bg-muted text-primary rounded-xl" : ""
         } ${
-          role === "bot" ? "flex flex-col gap-4 border-l border-primary/40" : ""
+          role === "assistant"
+            ? "flex flex-col gap-4 border-l border-primary/30"
+            : ""
         } py-2 px-3 inline-block text-base max-w-full`}
       >
-        <MDEditor.Markdown
-          source={content}
-          style={{ whiteSpace: "pre-wrap" }}
-        />
+        <MDEditor.Markdown source={content} />
       </div>
     </div>
   );
