2bd18141a96a17fd27b52ac742434a550e7fcd95
moashraf86 <botoashraf123@gmail.com>
2024-08-30 21:20:39 +0300

    style: update chatIInput design

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -22,6 +22,7 @@ export const ChatInput = ({ onSend }) => {
   const form = useForm({
     defaultValues: { input: "" },
     resolver: zodResolver(schema),
+    mode: "onChange",
   });
 
   /**
@@ -30,44 +31,58 @@ export const ChatInput = ({ onSend }) => {
   const onSubmit = (message) => {
     onSend(message.input);
     form.reset();
-    // navigate to the bottom of the chat
-    window.scrollTo(0, document.body.scrollHeight);
     // reset the textarea height
     const textarea = document.querySelector("textarea");
     textarea.style.height = "auto";
   };
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex gap-3 items-end fixed bottom-0 left-0 right-0 p-4 bg-background max-w-3xl mx-auto"
-      >
-        <FormField
-          control={form.control}
-          name="input"
-          render={({ field }) => (
-            <FormItem className="grow">
-              <FormControl>
-                <Textarea
-                  className="bg-primary-foreground"
-                  placeholder="Message GroqBot..."
-                  {...field}
-                  onInput={(e) => {
-                    field.onChange(e);
-                    const textarea = e.target;
-                    textarea.style.height = "auto";
-                    textarea.style.height = textarea.scrollHeight + "px";
-                  }}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <Button type="submit">
-          <PaperPlaneIcon />
-        </Button>
-      </form>
-    </Form>
+    <div className="fixed bottom-0 left-0 right-0 mx-auto bg-background z-50 flex flex-col gap-2 p-3">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex items-end gap-3 p-2 max-w-3xl mx-auto w-full bg-primary-foreground rounded-[1.75rem]"
+        >
+          <FormField
+            control={form.control}
+            name="input"
+            render={({ field }) => (
+              <FormItem className="grow">
+                <FormControl>
+                  <Textarea
+                    className="border-none bg-transparent w-full focus-visible:ring-0"
+                    placeholder="Message GroqBot..."
+                    {...field}
+                    onInput={(e) => {
+                      field.onChange(e);
+                      const textarea = e.target;
+                      textarea.style.height = "auto";
+                      textarea.style.height = textarea.scrollHeight + "px";
+                    }}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter" && !e.shiftKey) {
+                        e.preventDefault();
+                        form.handleSubmit(onSubmit)();
+                      }
+                    }}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button
+            className="rounded-full p-0 w-10 h-10 disabled:opacity-30"
+            type="submit"
+            disabled={!form.formState.isValid}
+          >
+            <PaperPlaneIcon />
+          </Button>
+        </form>
+        {/* Copy right footer */}
+      </Form>
+      <div className="text-center text-xs text-primary/50">
+        © 2024 GroqBot. All rights reserved.
+      </div>
+    </div>
   );
 };
