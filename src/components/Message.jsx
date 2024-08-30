927432713a0c21928e0111f26ef921c96dad371f
moashraf86 <botoashraf123@gmail.com>
2024-08-30 21:22:30 +0300

    refactor: add lastMsgRef

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,17 +1,19 @@
+/* eslint-disable react/prop-types */
 import MDEditor from "@uiw/react-md-editor";
-export const Message = ({ content, role }) => {
+export const Message = ({ content, role, lastMsgRef, index }) => {
   console.log(content, role);
 
   return (
     <div className={`flex ${role === "user" ? "justify-end" : ""}`}>
       <div
+        ref={role === "assistant" && index !== 0 ? lastMsgRef : null}
         className={` ${
-          role === "user" ? "bg-muted text-primary rounded-xl" : ""
+          role === "user" ? "bg-muted text-primary rounded-xl mt-10" : ""
         } ${
           role === "assistant"
             ? "flex flex-col gap-4 border-l border-primary/30"
             : ""
-        } py-2 px-3 inline-block text-base max-w-full`}
+        } py-2 px-4 inline-block text-base max-w-full`}
       >
         <MDEditor.Markdown source={content} />
       </div>
