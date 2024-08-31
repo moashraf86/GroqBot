256f22e2552659b170c1dcaab99354ce1760203b
moashraf86 <botoashraf123@gmail.com>
2024-08-31 22:02:16 +0300

    style: remove shadow from textarea component

--- src/components/ui/textarea.jsx
+++ src/components/ui/textarea.jsx
@@ -8,7 +8,7 @@ const Textarea = React.forwardRef(({ className, ...props }, ref) => {
     <textarea
       rows={1}
       className={cn(
-        "flex w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 resize-none overflow-hidden",
+        "flex w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 resize-none overflow-hidden",
         className
       )}
       ref={ref}
