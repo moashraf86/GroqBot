cc8a9b8f7aecbed6b0d7a37e98f1617eb5ba4b7e
moashraf86 <botoashraf123@gmail.com>
2024-09-05 15:58:18 +0300

    feat: Add functionality to copy response to clipboard and stop response generation
    - Implemented a feature to copy the response text to the clipboard.
    - Added a mechanism to stop the generation of responses when requested.

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,7 +1,12 @@
 /* eslint-disable react/prop-types */
 import { Message } from "./Message";
 
-export const ChatBox = ({ messages, loading, currentMsgIndex }) => {
+export const ChatBox = ({
+  messages,
+  loading,
+  currentMsgIndex,
+  isGenerating,
+}) => {
   return (
     <div className="flex flex-col gap-8 grow pb-24">
       {messages.map((message, index) => (
@@ -9,6 +14,8 @@ export const ChatBox = ({ messages, loading, currentMsgIndex }) => {
           key={index}
           {...message}
           loading={loading && index === currentMsgIndex}
+          lastMsg={index === messages.length - 1}
+          isGenerating={isGenerating}
         />
       ))}
     </div>
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,16 @@
 /* eslint-disable react/prop-types */
 import MDEditor from "@uiw/react-md-editor";
 import { Button } from "./ui/button";
-import { Pencil1Icon } from "@radix-ui/react-icons";
+import { CopyIcon, Pencil1Icon } from "@radix-ui/react-icons";
 import { Skeleton } from "./ui/skeleton";
-export const Message = ({ content, role, loading }) => {
+export const Message = ({ content, role, loading, isGenerating, lastMsg }) => {
+  /**
+   * Copy message to clipboard on button click
+   */
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(content);
+  };
+
   return (
     <>
       {role === "user" ? (
@@ -21,17 +28,56 @@ export const Message = ({ content, role, loading }) => {
         </div>
       ) : (
         // Assistant message
-        <div className="flex">
-          <div className="flex flex-col gap-4 border-l py-2 pl-5 pr-4 text-base min-w-full max-w-full border-primary/30">
+        <div className="grid gap-4 group">
+          <div className="flex flex-col gap-4 py-1 pr-4 text-base min-w-full max-w-full">
             {loading ? (
-              <div className="flex flex-col gap-2">
+              <div className="flex flex-col gap-2 border-l border-primary/30 pl-4">
                 <Skeleton className="w-3/4 h-3" />
                 <Skeleton className="w-1/2 h-3" />
               </div>
             ) : (
-              <MDEditor.Markdown source={content} />
+              <div className="border-l border-primary/30 pl-4">
+                <MDEditor.Markdown source={content} />
+              </div>
             )}
           </div>
+          {/* Response Toolbar */}
+          {!lastMsg ? (
+            <div className="opacity-0 group-hover:opacity-100 flex gap-2">
+              <Button
+                title="Copy"
+                variant="ghost"
+                size="icon"
+                className="w-7 h-7"
+                onClick={copyToClipboard}
+              >
+                <CopyIcon
+                  width={16}
+                  height={16}
+                  className="inline-block text-primary/70"
+                />
+              </Button>
+            </div>
+          ) : (
+            <div
+              className={`flex gap-2 ${
+                content && !isGenerating ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <Button
+                variant="ghost"
+                size="icon"
+                className="w-7 h-7"
+                onClick={copyToClipboard}
+              >
+                <CopyIcon
+                  width={16}
+                  height={16}
+                  className="inline-block text-primary/70"
+                />
+              </Button>
+            </div>
+          )}
         </div>
       )}
     </>
