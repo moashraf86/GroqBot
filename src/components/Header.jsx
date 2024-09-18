4a2698ac4d7857123f30bff0b45bdacba4f01c5a
moashraf86 <botoashraf123@gmail.com>
2024-09-18 20:07:48 +0300

    refactor: add `logo` to bot response and `checkIcon` for copied indication

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,10 +34,10 @@ export const Header = ({ setIsGenerating }) => {
   };
 
   return (
-    <header className="sticky inset-0 z-50 bg-background/50 backdrop-blur py-4 border-b border-border">
+    <header className="sticky inset-0 z-50 bg-background py-4 border-b border-border">
       <div className="container md:px-8 flex items-center justify-between">
         <h1 className="text-2xl font-bold uppercase text-primary">
-          <span className="text-accent font-open font-black ">//</span>
+          <span className="text-accent font-open font-black ">{"//"}</span>
           GroqBot
         </h1>
         <div className="flex items-center gap-2">
